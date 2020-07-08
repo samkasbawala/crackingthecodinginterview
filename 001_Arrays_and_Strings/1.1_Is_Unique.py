58beb0a75d80197ac6322676e6c9ef186c00b6c9
Sam Kasbawala <samarth.kasbawala@uconn.edu>
2020-07-08 15:30:25 -0400

    1.1 is unique

---
+++ 001_Arrays_and_Strings/1.1_Is_Unique.py
@@ -0,0 +1,57 @@
+__author__ = 'Sam Kasbawala'
+__credits__ = 'Sam Kasbawala'
+
+import unittest
+
+
+def is_unique(string: str, ds: bool = True) -> bool:
+    """Function to see if inputted string has all unique characters"""
+
+    # Using set data structure, O(n) runtime
+    if ds:
+        seen = set()
+        for char in string:
+            if char in seen:
+                return False
+            seen.add(char)
+        return True
+
+    # Using no additional data structures, O(n^2) runtime
+    else:
+        for i in range(len(string)):
+            for j in range(i+1, len(string)):
+                if string[i] == string[j]:
+                    return False
+        return True
+
+
+class TestIsUnique(unittest.TestCase):
+    def test_is_unique_false(self):
+        assert is_unique('sam kasbawala') is False
+        assert is_unique('test') is False
+        assert is_unique('unique') is False
+        assert is_unique('\n\n') is False
+        assert is_unique('\\\\') is False
+        
+        assert is_unique('sam kasbawala', ds=False) is False
+        assert is_unique('test', ds=False) is False
+        assert is_unique('unique', ds=False) is False
+        assert is_unique('\n\n', ds=False) is False
+        assert is_unique('\\\\', ds=False) is False
+
+    def test_is_unique_true(self):
+        assert is_unique('sam') is True
+        assert is_unique('qwerty') is True
+        assert is_unique('\n\tt') is True
+        assert is_unique('2 words') is True
+        assert is_unique('coding') is True
+
+        assert is_unique('sam', ds=False) is True
+        assert is_unique('qwerty', ds=False) is True
+        assert is_unique('\n\tt', ds=False) is True
+        assert is_unique('2 words', ds=False) is True
+        assert is_unique('coding', ds=False) is True
+
+
+if __name__ == '__main__':
+    unittest.main()
--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Cracking the Coding Interview
-------
 This repo is designed to help me prepare for upcoming coding interviews. In this repo, you will find ***my attempts*** to solving various interview questions. This repo is following the 6th edition of "Cracking the Coding Interview". Note that not every question will be solved. If you stumble across this repo, it might help you get an idea of some of the questions to expect in an interview. This repo is not a replacement for the book or any other medium of studying; it's simply an aid. It allows me to put my code in one place and allows me to access it wherever I go. For the small number of you who do come across this repo, feel free to fork it and add your own spin and don't hesitate to make a pull request if you think I made an error. Happy studying :)
+
+## Contents:
+[Chapter 1: Arrays and Strings](#arrays-and-strings)
+- [1.1: Is Unique](#1.1-is-unique)
+
+## Arrays and Strings
+In this chapter we will go over some interview questions regarding arrays and strings. Again, these questions are from the 6th edition of "Cracking the Coding Interview". In this chapter, each of the questions will be listed. You can find my code to each question in the subsequent chapter folders. Each question will have it's own python file.
+
+### 1.1 Is Unique
+Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
